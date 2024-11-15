9b82ab02750afc693a40854acc255b3e125c06df
Alan Shaiju Kurian <118761720+AlanShaiju@users.noreply.github.com>
2024-11-15 16:17:52 +0530

    Add files via upload

---
+++ Lab Experiments/test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+sem_t chopsticks[5];
+int *philosopher(void *arg)
+{
+    int id=*(int*)arg;
+    while(1)
+    {
+        sem_wait(&chopsticks[id%5]);
+        sem_wait(&chopsticks[((id+1)%5)]);
+        printf("Philosopher %d is eating...\n",id);
+        sem_post(&chopsticks[id%5]);
+        sem_post(&chopsticks[((id+1)%5)]);
+        printf("Philosopher %d is thinking...\n",id);
+        usleep(rand()%1000);
+    }
+}
+int main()
+{
+    pthread_t philosophers[5];
+    for(int i=0;i<5;i++)
+        sem_init(&chopsticks[i],0,1);
+    for(int i=0;i<5;i++)
+        pthread_create(&philosophers[i],NULL,philosopher,&i); 
+    for(int i=0;i<5;i++)
+        pthread_join(philosophers[i],NULL);
+    for(int i=0;i<5;i++)
+        sem_destroy(&chopsticks[i]);
+    return 0;
+}
